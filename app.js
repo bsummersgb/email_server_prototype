aa65c3fb4bd6f6f2a37f8ce48f16048e87c3cd09
bsummersgb <bsummersgb@gmail.com>
2018-05-28 16:55:49 +0800

    Installed handlebars and rendered post data as html

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const emails = require('./routes/emails');
 const app = express();
+const hbs = require('express-handlebars');
+
+// view engine setup
+app
+    .engine('hbs', hbs({extname: 'hbs', layoutsDir: __dirname + '/views/email-templates/'}))
+    .set('views', __dirname + '/views')
+    .set('view engine', 'hbs')
 
 // use the router as middleware
-app
-    .use (bodyParser.json())
+    // .get('/', (req, res) => {
+    //     var bankAccount = 066616102;
+    //     res.render('email-templates/bank-details', {
+    //         bankAccount: bankAccount
+    //     })
+    // })
+    .use (bodyParser.json()) // destructure the payload into an variable and then render it in the view
     .use('/emails', emails);
 
 
--- routes/emails.js
+++ routes/emails.js
@@ -3,8 +3,13 @@ const router = express.Router();
 
 router
     .post('/', (req, res) => {
-        console.log(req.body);
-        res.send('ok');
+        var emailDetails = { ...req.body };
+        console.log(emailDetails);
+        res.render('email-templates/bank-details', {
+            name: emailDetails.name,
+            age: emailDetails.age
+        })
+        // res.send(emailDetails);
     })
 
 module.exports = router;
