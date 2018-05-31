02900f0b2ea0ab9bb29ce048be6a276d05ee92e0
bsummersgb <bsummersgb@gmail.com>
2018-05-31 13:41:22 +0800

    Added error handling, options for prod and dev, public folder

--- app.js
+++ app.js
@@ -1,24 +1,46 @@
+const createError = require('http-errors');
 const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const emails = require('./routes/emails');
-const app = express();
+const logger = require('morgan');
 const hbs = require('express-handlebars');
 
+
+const indexRouter = require('./routes/index');
+const templatesRouter = require('./routes/emails');
+
+const app = express();
+
 // view engine setup
-app
-    .engine('hbs', hbs({extname: 'hbs', layoutsDir: __dirname + '/views/email-templates/'}))
-    .set('views', __dirname + '/views')
-    .set('view engine', 'hbs')
+app.engine('hbs', hbs({extname: 'hbs', layoutsDir: __dirname + '/views/'}))
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
-// use the router as middleware
-    // .get('/', (req, res) => {
-    //     var bankAccount = 066616102;
-    //     res.render('email-templates/bank-details', {
-    //         bankAccount: bankAccount
-    //     })
-    // })
-    .use (bodyParser.json()) // destructure the payload into an variable and then render it in the view
-    .use('/emails', emails);
+app.use(logger('dev'));
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
+app.use('/emails', templatesRouter);
 
-app.listen(3000);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
--- routes/emails.js
+++ routes/emails.js
@@ -1,15 +1,22 @@
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
 
+/* GET email templates */
 router
-    .post('/', (req, res) => {
-        var emailDetails = { ...req.body };
-        console.log(emailDetails);
-        res.render('email-templates/bank-details', {
-            name: emailDetails.name,
-            age: emailDetails.age
-        })
-        // res.send(emailDetails);
+  .get('/', function(req, res, next) {
+    res.send('respond with a resource');
+  })
+  .post('/', (req, res) => {
+    var emailDetails = { ...req.body };
+    console.log(emailDetails);
+    res.render('layout', {
+        name: emailDetails.name,
+        age: emailDetails.age,
+        partials: {
+          bankDetails: "bank-details"
+        }
     })
+    // res.send(emailDetails);
+  })
 
 module.exports = router;
